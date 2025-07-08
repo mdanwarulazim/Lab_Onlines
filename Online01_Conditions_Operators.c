d363e602802b80a57c7a6f22108454c01337e107
mdanwarulazim <anwarulazim.professional@gmail.com>
2025-07-08 09:10:10 -0700

    Create Online01_Conditions_Operators.c

---
+++ Online01_Conditions_Operators.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main (void)
+{
+    int a,b,c,sum;
+
+    printf("Enter the angles: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    sum=a+b+c;
+
+    if(sum!=180){
+        printf("The angles %d %d %d will not form a valid triangle.", a,b,c);
+    }
+    else if(a==90 || b==90 || c==90){
+        printf("The angles %d %d %d will form a right triangle.", a,b,c);
+    }
+    else if (a<90 && b<90 && c<90){
+        printf("The angles %d %d %d will form an acute triangle.", a,b,c);
+    }
+        else if (a>90 || b>90 || c>90){
+        printf("The angles %d %d %d will form an obtuse triangle.", a,b,c);
+    }
+
+
+    return 0;
+}
